6b4e43303f38d9f598cb332e61f36bf956630417
walid <bousnina.walid1@gmail.com>
2018-04-01 18:25:11 +0200

    to chams

--- src/OM/AdministrationBundle/Controller/TagController.php
+++ src/OM/AdministrationBundle/Controller/TagController.php
@@ -33,10 +33,16 @@ class TagController extends FOSRestController
         $data = new Tag();
         $nom = $request->get('nom');
         $color = $request->get('color');
+        $work = $request->get('work');
 
 
         $data->setNom($nom);
         $data->setColor($color);
+        if($work=="true"){
+            $data->setWork(true);
+        }else{
+            $data->setWork(false);
+        }
 
         //var_dump($data);die();
         $em = $this->getDoctrine()->getManager();
@@ -64,10 +70,37 @@ class TagController extends FOSRestController
         $formatted = [];
         foreach ($restresult as $result) {
             $formatted[] = [
-
+                'id'=>$result->getId(),
                 'nom' => $result->getNom(),
+                'color' => $result->getColor(),
+                'work' => $result->getWork(),
 
             ];
+        }
+
+        return new JsonResponse($formatted);
+    }
+    /**
+     * @Rest\Get("/api/tagactive", name="_alltags")
+     *
+     */
+    public function getTagactiveAction()
+    {
+        $restresult = $this->getDoctrine()->getRepository('OMAdministrationBundle:Tag')->findAll();
+        if ($restresult === null) {
+            return new View("Pas de tag disponible", Response::HTTP_NOT_FOUND);
+        }
+        $formatted = [];
+        foreach ($restresult as $result) {
+            if($result->getWork()) {
+                $formatted[] = [
+                    'id' => $result->getId(),
+                    'nom' => $result->getNom(),
+                    'color' => $result->getColor(),
+                    'work' => $result->getWork(),
+
+                ];
+            }
         }
 
         return new JsonResponse($formatted);
@@ -83,14 +116,22 @@ class TagController extends FOSRestController
     {
         $data = new Tag();
         $name = $request->get('nom');
+        $color = $request->get('color');
+        $work= $request->get('work');
         $em = $this->getDoctrine()->getManager();
         $tag = $this->getDoctrine()->getRepository('OMAdministrationBundle:Tag')->find($id);
         if (empty($tag)) {
             $view = new View("Tag not found", Response::HTTP_NOT_FOUND);
             return $this->handleView($view);
         }
-        elseif(!empty($name) ){
+        elseif(!empty($tag) ){
             $tag->setNom($name);
+            $tag->setColor($color);
+            if($work=="true")
+            $tag->setWork(true);
+            else
+                $tag->setWork(false);
+
             $em->flush();
             $view =new View("Tag Updated Successfully", Response::HTTP_OK);
             return $this->handleView($view);
@@ -99,6 +140,36 @@ class TagController extends FOSRestController
         $view = new View("Name cannot be empty", Response::HTTP_NOT_ACCEPTABLE);
 
          return $this->handleView($view);
+        }
+    }
+    /**
+     * @Rest\Put("api/{id}/tagonoff")
+     * @param $id
+     * @param Request $request
+     * @return Response
+     */
+    public function onoffAction($id,Request $request)
+    {
+        $data = new Tag();
+        $em = $this->getDoctrine()->getManager();
+        $tag = $this->getDoctrine()->getRepository('OMAdministrationBundle:Tag')->find($id);
+        if (empty($tag)) {
+            $view = new View("Tag not found", Response::HTTP_NOT_FOUND);
+            return $this->handleView($view);
+        }
+        elseif(!empty($tag) ){
+            if($tag->getWork())
+            $tag->setWork(false);
+            else
+            $tag->setWork(true);
+            $em->flush();
+            $view =new View("Tag Updated Successfully", Response::HTTP_OK);
+            return $this->handleView($view);
+        }
+        else{
+            $view = new View("Name cannot be empty", Response::HTTP_NOT_ACCEPTABLE);
+
+            return $this->handleView($view);
         }
     }
 
