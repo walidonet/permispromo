0b02b2c6a66375940c78f0e764097ba0306e0fe7
DESKTOP-DFLEMQ3\pc <chamseddine.bezzaouia@gmail.com>
2018-05-11 13:11:11 +0100

    commit product entity + product crud

---
+++ src/OM/AdministrationBundle/Controller/ProductController.php
@@ -0,0 +1,210 @@
+<?php
+
+namespace OM\AdministrationBundle\Controller;
+
+
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\View\View;
+use OM\AdministrationBundle\Entity\Product;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProductController extends FOSRestController
+{
+    public function indexAction($name)
+    {
+        return $this->render('', array('name' => $name));
+    }
+
+    /**
+     * @Rest\Get("/api/product", name="_allproducts")
+     *
+     */
+    public function getProductAction()
+    {
+        $restresult = $this->getDoctrine()->getRepository('OMAdministrationBundle:Product')->findAll();
+        if ($restresult === null) {
+            return new View("Pas de produits disponibles", Response::HTTP_NOT_FOUND);
+        }
+        $formatted = [];
+        foreach ($restresult as $result) {
+            $formatted[] = [
+
+                'libelle' => $result->getLibelle(),
+                'work'=> $result->getWork(),
+                'mode' => $result->getMode(),
+                'modality'=> $result->getModality(),
+                'offre'=> $result->getOffre(),
+                'contract'=> $result->getContract(),
+
+            ];
+        }
+
+        return new JsonResponse($formatted);
+    }
+
+
+    /**
+     * @Rest\Post("/api/product", name="product")
+     * @param Request $request
+     * @return Response
+     */
+
+    public function postProductAction(Request $request)
+    {
+
+        $data = new Product();
+        $libelle = $request->get('libelle');
+        $work = $request->get('work');
+        $idmode = $request->get('mode');
+        $idmodality = $request->get('modality');
+        $idoffre = $request->get('offre');
+        $idcontract = $request->get('contract');
+
+        $em= $this->getDoctrine()->getManager();
+
+        $mode = $em->getRepository('OMAdministrationBundle:PaymentMode')->find($idmode);
+        $modality = $em->getRepository('OMAdministrationBundle:PaymentModality')->find($idmodality);
+        $offre = $em->getRepository('OMAdministrationBundle:Offre')->find($idoffre);
+        $contract = $em->getRepository('OMAdministrationBundle:Contract')->find($idcontract);
+
+
+
+
+        $data->setLibelle($libelle);
+        if($work=="true"){
+            $data->setWork(true);
+        }else{
+            $data->setWork(false);
+        }
+        $data->setMode($mode);
+        $data->setModality($modality);
+        $data->setOffre($offre);
+        $data->setContract($contract);
+
+        $em->persist($data);
+        $em->flush();
+        $view =new View("product Added Successfully", Response::HTTP_OK);
+
+        return $this->handleView($view);
+    }
+
+
+    /**
+     * @Rest\Put("api/{id}/product")
+     * @param $id
+     * @param Request $request
+     * @return Response
+     */
+    public function updateAction($id,Request $request)
+    {
+        $data = new Product();
+        $libelle = $request->get('libelle');
+        $work = $request->get('work');
+        $mode = $request->get('mode');
+        $modality = $request->get('modality');
+        $offre = $request->get('offre');
+        $contract = $request->get('contract');
+
+        $em= $this->getDoctrine()->getManager();
+
+        $em = $this->getDoctrine()->getManager();
+        $product = $this->getDoctrine()->getRepository('OMAdministrationBundle:Product')->find($id);
+        if (empty($product)) {
+            $view = new View("Product not found", Response::HTTP_NOT_FOUND);
+            return $this->handleView($view);
+        }
+        elseif(!empty($product) ){
+            $data->setLibelle($libelle);
+            if($work=="true"){
+                $data->setWork(true);
+            }else{
+                $data->setWork(false);
+            }
+            $data->setMode($mode);
+            $data->setModality($modality);
+            $data->setOffre($offre);
+            $data->setContract($contract);
+
+            $em->flush();
+            $view =new View("Product Updated Successfully", Response::HTTP_OK);
+            return $this->handleView($view);
+        }
+        else{
+            $view = new View("Product cannot be empty", Response::HTTP_NOT_ACCEPTABLE);
+
+            return $this->handleView($view);
+        }
+    }
+
+    /**
+     * @Rest\Put("api/{id}/productonoff")
+     * @param $id
+     * @param Request $request
+     * @return Response
+     */
+    public function onoffProductAction($id,Request $request)
+    {
+        $data = new Product();
+        $em = $this->getDoctrine()->getManager();
+        $product = $this->getDoctrine()->getRepository('OMAdministrationBundle:Product')->find($id);
+        if (empty($product)) {
+            $view = new View("Product not found", Response::HTTP_NOT_FOUND);
+            return $this->handleView($view);
+        } elseif (!empty($product)) {
+            if ($product->getWork())
+                $product->setWork(false);
+            else
+                $product->setWork(true);
+            $em->flush();
+            $view = new View("Product Updated Successfully", Response::HTTP_OK);
+            return $this->handleView($view);
+        } else {
+            $view = new View("Product cannot be empty", Response::HTTP_NOT_ACCEPTABLE);
+
+            return $this->handleView($view);
+        }
+    }
+
+
+        /**
+         * @Rest\Delete("api/{id}/product")
+         * @param $id
+         * @return Response
+         */
+        public function deleteAction($id)
+    {
+        $data = new Product();
+        $em = $this->getDoctrine()->getManager();
+        $product = $this->getDoctrine()->getRepository('OMAdministrationBundle:Product')->find($id);
+        if (empty($product)) {
+            $view = new View("product not found", Response::HTTP_NOT_FOUND);
+            return $this->handleView($view);
+        }
+        else {
+            $em->remove($product);
+            $em->flush();
+        }
+        $view = new View("product deleted successfully", Response::HTTP_OK);
+        return $this->handleView($view);
+    }
+
+    /**
+     * @Rest\Get("api/product/{id}")
+     * @param $id
+     * @return View|object|Product
+     */
+    public function getProductByIdAction($id)
+    {
+        $singleresult = $this->getDoctrine()->getRepository('OMAdministrationBundle:Product')->find($id);
+        if ($singleresult === null) {
+            return new View("product not found", Response::HTTP_NOT_FOUND);
+        }
+        return $singleresult;
+    }
+
+
+}
---
+++ src/OM/AdministrationBundle/Entity/Product.php
@@ -0,0 +1,180 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pc
+ * Date: 09/05/2018
+ * Time: 17:30
+ */
+
+namespace OM\AdministrationBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ *
+ *
+ *
+ * @ORM\Table(name="product")
+ * @ORM\Entity
+ */
+class Product
+{
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $libelle ;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $work ;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="OM\AdministrationBundle\Entity\PaymentMode",inversedBy="mode", cascade={"persist","merge", "remove"})
+     */
+    private $mode;
+
+    /**
+     *
+     * @ORM\ManyToOne(targetEntity="OM\AdministrationBundle\Entity\PaymentModality",inversedBy="modality", cascade={"persist", "remove"})
+     */
+    private $modality;
+
+
+    /**
+     *
+     * @ORM\ManyToOne(targetEntity="OM\AdministrationBundle\Entity\Offre", inversedBy="offre",cascade={"persist", "remove"})
+     */
+    private $offre;
+
+    /**
+     *
+     * @ORM\ManyToOne(targetEntity="OM\AdministrationBundle\Entity\Contract", inversedBy="contract",cascade={"persist", "remove"})
+     */
+    private $contract;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLibelle()
+    {
+        return $this->libelle;
+    }
+
+    /**
+     * @param mixed $libelle
+     */
+    public function setLibelle($libelle)
+    {
+        $this->libelle = $libelle;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getWork()
+    {
+        return $this->work;
+    }
+
+    /**
+     * @param mixed $work
+     */
+    public function setWork($work)
+    {
+        $this->work = $work;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMode()
+    {
+        return $this->mode;
+    }
+
+    /**
+     * @param mixed $mode
+     */
+    public function setMode($mode)
+    {
+        $this->mode = $mode;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getModality()
+    {
+        return $this->modality;
+    }
+
+    /**
+     * @param mixed $modality
+     */
+    public function setModality($modality)
+    {
+        $this->modality = $modality;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOffre()
+    {
+        return $this->offre;
+    }
+
+    /**
+     * @param mixed $offre
+     */
+    public function setOffre($offre)
+    {
+        $this->offre = $offre;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContract()
+    {
+        return $this->contract;
+    }
+
+    /**
+     * @param mixed $contract
+     */
+    public function setContract($contract)
+    {
+        $this->contract = $contract;
+    }
+
+
+
+
+
+}
